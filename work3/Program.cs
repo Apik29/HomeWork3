cbfb79c9a02c59da56e547407255ed754160ac34
Apik29 <121308884+Apik29@users.noreply.github.com>
2023-01-25 15:39:15 +0300

    3ok

--- work3/Program.cs
+++ work3/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int num = new Random().Next(1, 21);
+System.Console.WriteLine( $"num = {num}");
+
+for (int i = 1; i <= num; i++)
+{
+    System.Console.WriteLine(Math.Pow(i, 3));
+}
