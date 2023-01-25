798f40ca1f4343b20a9738d6352f9b92375d750d
Apik29 <121308884+Apik29@users.noreply.github.com>
2023-01-25 15:27:27 +0300

    2ok

--- work2/Program.cs
+++ work2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Random rnd = new Random();
+int x1 = rnd.Next(-10, 11);
+int y1 = rnd.Next(-10, 11);
+int x2 = rnd.Next(-10, 11);
+int y2 = rnd.Next(-10, 11);
+int z1 = rnd.Next(-10, 11);
+int z2 = rnd.Next(-10, 11);
+System.Console.WriteLine($"x1 {x1}, y1 {y1}, x2 {x2}, y2 {y2}, z1 {z1}, z2 {z2}");
+
+double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z1 - z2, 2));
+
+System.Console.WriteLine(distance);
